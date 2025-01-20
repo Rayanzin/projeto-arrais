f88cbdab52b3cefa0ac0cec364deae4b3f1afd44
Rayan <139246019+Rayanzin@users.noreply.github.com>
2025-01-20 12:04:10 -0300

    projeto arrais veiculos iniciado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
+import { Button } from "antd";
+
 const Home = () => {
     return (
-        <div className="w-full h-[50px] flex justify-end items-center">
-            <button>Login</button>
-        </div>
+        <>
+            <div className="w-full h-[50px] flex justify-end items-center">
+                <Button type="primary" size="large" className="bg-laranja hover:!bg-laranja2">
+                    <a href="/login">Login</a>
+                </Button>
+            </div>
+            <section>
+                <h1 className="text-2xl font-bold mb-4">Tabela de veículos</h1>
+
+            </section>
+        </>
     );
 }
- 
+
 export default Home;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { Input } from "antd";
+
+const Login = () => {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <form className="w-1/4 p-4 border rounded-md">
+                <Input className="mb-3"
+                    placeholder="Enter your username"
+                    prefix={
+                        <UserOutlined className="text-black/25"/>
+                    }
+                />
+                <Input.Password className="mb-3" placeholder="input password" />
+                <Button type="primary" className="bg-laranja hover:!bg-laranja2">
+                    <a href="/">Voltar</a>
+                </Button>
+                <Button type="primary" className="bg-laranja hover:!bg-laranja2">Entrar</Button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
